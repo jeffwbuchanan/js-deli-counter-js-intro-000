b6d035434e7929867b9e1c0ca0ab50e9be88c601
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-03-13 21:06:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(katzDeli, name){
   katzDeli.push(name);
   return "Welcome, ${name}. + You are number ${katzDeli.length} in line."
 };
+
+function nowServing(line){
+  if (line.length === 0){
+    console.log("There is no one waiting to be served.")
+    return "There is no one waiting to be served."
+  } else {
+    return "Now serving" + line.shift()"."
+  }
+};
